f8de8b65fb5812846b3af27d6b4ee7000459b634
Jimin Kim <0305jimin@gmail.com>
2023-05-13 15:10:52 +0900

    [FIX] build error #1

--- mument/src/main/java/com/mument/mument/MumentApplication.java
+++ mument/src/main/java/com/mument/mument/MumentApplication.java
@@ -2,8 +2,9 @@ package com.mument.mument;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class MumentApplication {
 
 	public static void main(String[] args) {
